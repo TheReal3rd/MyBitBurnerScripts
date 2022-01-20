147a5405abafc1188fe82a0252703fcfe7398cd4
3RD <60966545+TheReal3rd@users.noreply.github.com>
2022-01-20 20:03:47 +0000

    Fixes and Improvements

--- WormV1/Attack.js
+++ WormV1/Attack.js
@@ -3,79 +3,83 @@
 @Args Log(0,1) & Hostname
 @Description
 	-Automatically attack server to run hack-lite.script.
-@Mem 2.45
+@Mem 4.45
 */
 
-//Args Check
+// Args Check
 if (args.length == 0) {
 	tprint("Required args not provided.")
 	exit()
 }
 
-//Vars
+// Vars
 var log = (args[0] === 1)
-var ftpC = fileExists("FTPCrack.exe")
-var bSSH = fileExists("BruteSSH.exe")
-var rSMTP = fileExists("relaySMTP.exe")
-var wHTTP = fileExists("HTTPWorm.exe")
-var iSQL = fileExists("SQLInject.exe")
 
 // Server Foreach
-for (var i = 1; i != args.length; i++) {
-	var target = args[i]
+var serversList = []
+if (args[1].includes(',')) serversList = args[1].split(',')
+else serversList.push(args[1])
+
+for (var i = 0; i != serversList.length; i++) {
+	var target = serversList[i]
+	logMessage(log, "Target: " + target)
+
 	// Port Attacks START
 	var aInc = 0;
+	var pAttacks = 0;//Port Attacked
 	var closing = false
 	while (!closing) {
-		if (getServerNumPortsRequired(target) > aInc) {
-			switch (aInc) {
-				case 0:
-					if (bSSH) {
-						logMessage(log, "Attacking With BruteSSH.")
-						brutessh(target)
-					}
-					aInc++;
-					break
-				case 1:
-					if (ftpC) {
-						logMessage(log, "Attacking With FTPCrack.")
-						ftpcrack(target)
-					}
-					aInc++;
-					break
-				case 2:
-					if (rSMTP) {
-						logMessage(log, "Attacking With RelaySMTP.")
-						relaysmtp(target)
-					}
-					aInc++;
-					break
-				case 3:
-					if (wHTTP) {
-						logMessage(log, "Attacking With HTTPWorm.")
-						httpworm(target)
-					}
-					aInc++;
-					break
-				case 4:
-					if (iSQL) {
-						logMessage(log, "Attacking With SQLInject.")
-						sqlinject(target)
-					}
-					aInc++;
-					break
-				case 5:
-					logMessage(log, "Ran out of attacks.")
-					closing = true
-					break
-			}
-		} else {
-			//TEMP FIX
-			if (getHackingLevel() >= getServerRequiredHackingLevel(target)) {
-				logMessage(log, "Attempting to gain Root Access...")
-				nuke(target)
-				logMessage(log, hasRootAccess(target) ? "Successfully gained root access!" : "Failed to gain root access!")
-			}
+		switch (aInc) {
+			case 0:
+				if (fileExists("BruteSSH.exe", "home")) {
+					logMessage(log, "Attacking With BruteSSH.")
+					brutessh(target)
+					pAttacks++;
+				}
+				aInc++;
+				break
+			case 1:
+				if (fileExists("FTPCrack.exe", "home")) {
+					logMessage(log, "Attacking With FTPCrack.")
+					ftpcrack(target)
+					pAttacks++;
+				}
+				aInc++;
+				break
+			case 2:
+				if (fileExists("relaySMTP.exe", "home")) {
+					logMessage(log, "Attacking With RelaySMTP.")
+					relaysmtp(target)
+					pAttacks++;
+				}
+				aInc++;
+				break
+			case 3:
+				if (fileExists("HTTPWorm.exe", "home")) {
+					logMessage(log, "Attacking With HTTPWorm.")
+					httpworm(target)
+					pAttacks++;
+				}
+				aInc++;
+				break
+			case 4:
+				if (fileExists("SQLInject.exe", "home")) {
+					logMessage(log, "Attacking With SQLInject.")
+					sqlinject(target)
+					pAttacks++;
+				}
+				aInc++;
+				break
+			case 5:
+				logMessage(log, "Number of port attacks done: " + pAttacks)
+				closing = true
+				break
+		}
+
+		if (getHackingLevel() >= getServerRequiredHackingLevel(target) && pAttacks >= getServerNumPortsRequired(target)) {
+			logMessage(log, "Attempting to gain root access...")
+			nuke(target)
+			logMessage(log, "Successfully gained root access!")
 			closing = true
 		}
 	}
@@ -89,6 +93,7 @@ for (var i = 1; i != args.length; i++) {
 	}
 	// Hack Level Attack END
 }
+spawn("attack-worm.script", 1, args[0])
 
 function logMessage(log, message) {
 	if (log) tprint(message)
